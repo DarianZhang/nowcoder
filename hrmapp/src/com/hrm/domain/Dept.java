dbfe875f555232964a3584439fa2f1686eec4e7a
DarianZhang <15970607112@163.com>
2018-09-05 10:14:08 +0800

    hrmapp
    Spring+Mybatis企业应用实战
    人事管理系统

---
+++ hrmapp/src/com/hrm/domain/Dept.java
@@ -0,0 +1,42 @@
+package com.hrm.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author 张建伟
+ * 持久化实体类
+ */
+public class Dept implements Serializable{
+
+	private Integer id;		// id
+	private String name;	// 部门名称
+	private String remark;	// 详细描述
+	// 无参数构造器
+	public Dept() {
+		super();
+	}
+	// setter和getter方法
+	public void setId(Integer id){
+		this.id = id;
+	}
+	public Integer getId(){
+		return this.id;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setRemark(String remark){
+		this.remark = remark;
+	}
+	public String getRemark(){
+		return this.remark;
+	}
+	@Override
+	public String toString() {
+		return "Dept [id=" + id + ", name=" + name + ", remark=" + remark + "]";
+	}
+
+}
